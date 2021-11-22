75a5e1c67ab921f6223d1a6976f978966ff1e9f3
StephhyL <Stephanie Li>
2021-11-22 13:43:41 -0500

    add COMPASS solutions

--- 2_queries_joins/2_cohorts_total_assignment_duration.sql
+++ 2_queries_joins/2_cohorts_total_assignment_duration.sql
@@ -7,3 +7,10 @@ JOIN assignment_submissions ON assignment_submissions.student_id = students.id
 JOIN cohorts ON cohorts.id = students.cohort_id
 GROUP BY cohorts.name
 HAVING cohorts.name = 'FEB12';
+
+--COMPASS SOLUTION:
+-- SELECT sum(assignment_submissions.duration) as total_duration
+-- FROM assignment_submissions
+-- JOIN students ON students.id = student_id
+-- JOIN cohorts ON cohorts.id = cohort_id
+-- WHERE cohorts.name = 'FEB12';
